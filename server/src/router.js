8c6c7dfa33f8651fde7b717c24cdbe90f3809920
4nth0ny1 <acatullo4@gmail.com>
2023-08-06 12:33:24 -0500

    refactor router.js

--- server/src/router.js
+++ server/src/router.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const isLoggedIn = require("./middleware/isLoggedIn");
 
+const deleteTodoRoute = require("./routes/deleteTodoRoute");
+const updateTodoRoute = require("./routes/updateTodoRoute");
+const createTodoRoute = require("./routes/createTodoRoute");
+const readTodosRoute = require("./routes/readTodosRoute");
+
 const router = express.Router();
 
 router.post("/login", require("./routes/loginRoute"));
 
-router.get("/todos", isLoggedIn, require("./routes/readTodosRoute"));
-router.post("/todos", isLoggedIn, require("./routes/createTodoRoute"));
-router.put("/todos/:id", isLoggedIn, require("./routes/updateTodoRoute"));
-router.delete("/todos/:id", isLoggedIn, require("./routes/deleteTodoRoute"));
+router.get("/todos", isLoggedIn, readTodosRoute);
+router.post("/todos", isLoggedIn, createTodoRoute);
+router.put("/todos/:id", isLoggedIn, updateTodoRoute);
+router.delete("/todos/:id", isLoggedIn, deleteTodoRoute);
 
 module.exports = router;
