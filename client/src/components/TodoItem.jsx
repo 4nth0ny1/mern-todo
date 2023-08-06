200573fcdbc1fbe2c4e47f4d3711ec0a1c445317
4nth0ny1 <acatullo4@gmail.com>
2023-08-06 17:40:40 -0500

    refactored updateTodo on TodoItem and added a input field to update Text

--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -5,10 +5,8 @@ import updateTodoRequest from "../api/updateTodoRequest";
 const TodoItem = ({ todo }) => {
   const queryClient = useQueryClient();
 
-  const { mutate: toggleCompletion } = useMutation(
-    () => {
-      return updateTodoRequest({ ...todo, completed: !todo.completed });
-    },
+  const { mutate: updateTodo } = useMutation(
+    (updatedTodo) => updateTodoRequest(updatedTodo),
     {
       onSettled: () => {
         queryClient.invalidateQueries("todos");
@@ -21,7 +19,22 @@ const TodoItem = ({ todo }) => {
       <input
         checked={todo.completed}
         type="checkbox"
-        onChange={toggleCompletion}
+        onChange={() =>
+          updateTodo({
+            ...todo,
+            completed: !todo.completed,
+          })
+        }
+      />
+      <input
+        type="text"
+        value={todo.text}
+        onChange={(e) =>
+          updateTodo({
+            ...todo,
+            text: e.target.value,
+          })
+        }
       />
       {todo.text}
     </div>
