de4e74b9b529ac76781a0a42418dbf10aa200de6
4nth0ny1 <acatullo4@gmail.com>
2023-08-06 13:26:58 -0500

    added React Query, and render todos, added loading spinner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,25 @@
 import "./App.css";
 import readTodosRequest from "./api/readTodosRequest";
 import { useQuery } from "react-query";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
+  const { isLoading, data: todos } = useQuery("todos", readTodosRequest);
+
   return (
     <>
       <h1>Mern Todo App</h1>
-      {todos.map((todo) => {
-        return (
-          <div key={todo.id}>
-            {todo.text}: {todo.completed ? "Complete" : "Not Complete"}
-          </div>
-        );
-      })}
+      {isLoading ? (
+        <ClipLoader size={150} />
+      ) : (
+        todos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              {todo.text}: {todo.completed ? "Complete" : "Not Complete"}
+            </div>
+          );
+        })
+      )}
     </>
   );
 }
